50fb162ed5fdee71d83d5c7ca5f0df172d962de2
tynn <tynn.dev@gmail.com>
2014-07-27 01:40:54 +0200

    Use GPL license for non library modules

--- test/unit/__init__.py
+++ test/unit/__init__.py
@@ -3,16 +3,16 @@
 #	Copyright (c) 2014 Christian Schmitz <tynn.dev@gmail.com>
 #
 #	python-flac is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU Lesser General Public License as published by
+#	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
 #
 #	python-flac is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU Lesser General Public License for more details.
+#	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU Lesser General Public License
+#	You should have received a copy of the GNU General Public License
 #	along with python-flac.  If not, see <http://www.gnu.org/licenses/>.
 
 """ Unit tests for the flac package """
